33703590c49d9bc7c4268b2e17314bcc28a4b8ec
User <n/a>
2022-11-17 16:51:36 -0800

    pfp url

--- content.js
+++ content.js
@@ -1,5 +1,12 @@
 var url = new URL(window.location);
-if(url.toString().indexOf('theme=dark') == -1){
+if(url.toString().indexOf('/pfp') != -1){
+	url = new URL(url.toString().slice(0, url.toString().length-4)+"?__a=1&__d=dis");
+	var xmlHttp = new XMLHttpRequest();
+	xmlHttp.open("GET", url, false);
+	xmlHttp.send(null);
+	window.location.replace(new URL(xmlHttp.responseText["graphql"]["user"]["profile_pic_url_hd"]));
+}
+else if(url.toString().indexOf('theme=dark') == -1){
 	url.searchParams.append('theme', 'dark');
 	window.location.replace(url);
 }
